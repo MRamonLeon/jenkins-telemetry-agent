7cceb738d92ed39b827ee293ddc4eae3b59517df
Ramon Leon <manuelramonleonjimenez@gmail.com>
2018-12-28 11:32:01 +0100

    Java8 and clean up

--- src/main/java/jenkins/telemetry/agents/CatchExceptionTransformer.java
+++ src/main/java/jenkins/telemetry/agents/CatchExceptionTransformer.java
@@ -1,5 +1,6 @@
 package jenkins.telemetry.agents;
 
+import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtConstructor;
@@ -7,71 +8,62 @@ import javassist.LoaderClassPath;
 
 import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.security.ProtectionDomain;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CatchExceptionTransformer implements ClassFileTransformer {
+    private final static Logger LOGGER = Logger.getLogger(TelemetryAgent.class.getName());
 
-    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-        if(className.endsWith("ClassNotFoundException")){
-            System.out.format("Transforming '%s'%n", className);
-            //ClassPool classPool = new ClassPool();
-            //classPool.appendClassPath(new LoaderClassPath(loader));
+    private String className;
+    private ClassLoader classLoader;
+
+    public CatchExceptionTransformer(String className, ClassLoader classLoader) {
+        this.className = className.replaceAll("\\.", "/");
+        this.classLoader = classLoader;
+    }
+
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+        byte[] byteCode = classfileBuffer;
+
+        // This method is called at every class loaded, we transform only the specific class
+        // We have the class loader null (default) and WebAppClassLoader=Jenkins v2.157-SNAPSHOT@1f0f1111 (jenkins)
+        if (className.equals(this.className) /*&& loader.equals(this.classLoader)*/) {
+            LOGGER.log(Level.WARNING, "Transforming '%s'%n", classBeingRedefined);
 
             CtClass cl = null;
             try {
-                cl = createNewClassInJenkinsUberClassLoader(loader, classfileBuffer);
+                ClassPool classPool = ClassPool.getDefault();
+                classPool.appendClassPath(new LoaderClassPath(loader));
+                cl = classPool.makeClass(new java.io.ByteArrayInputStream(classfileBuffer));
 
-                if (cl.isInterface() == false) {
-                    // add a message in the constructors
-                    CtConstructor[] constr = cl.getDeclaredConstructors();
-                    for (CtConstructor con:constr) {
-                        con.insertAfter("System.out.println(\"A ClassNotFoundException has been created, cause:\" + getMessage()); printStackTrace();");
-                    }
-
-                    //It gives me:
-                    //Caused by: java.lang.UnsupportedOperationException: class redefinition failed: attempted to change the schema (add/remove fields)
-
-                    //add a field to test
-                    //CtField field = CtField.make("public String text = \"Hello world\";", cl);
-                    //cl.addField(field);
-
-                    //get the byte array of the class
-                    classfileBuffer = cl.toBytecode();
+                // add a message in the constructors
+                CtConstructor[] constructors = cl.getDeclaredConstructors();
+                for (CtConstructor con : constructors) {
+                    con.insertAfter("System.out.println(\"A ClassNotFoundException has been created, cause:\" + getMessage()); printStackTrace();");
                 }
-            } catch (Exception e) {
-                System.out.println(e);
+
+                //add a field to test
+                //CtField field = CtField.make("public String text = \"Hello world\";", cl);
+                //cl.addField(field)
+                //It gives me:
+                //Caused by: java.lang.UnsupportedOperationException: class redefinition failed: attempted to change the schema (add/remove fields)
+                //Because instrumentation doesn't allow changing fields, arguments on methods, and some other changes
+
+                //get the byte array of the class
+                byteCode = cl.toBytecode();
+
+            } catch (IOException | CannotCompileException e) {
+                LOGGER.log(Level.SEVERE, "Exception transforming " + classBeingRedefined, e);
             } finally {
                 if (cl != null) {
                     cl.detach();
                 }
             }
-        } else {
-            //System.out.format("Not transforming the class: '%s'%n", className);
-        }
+        }/*else{
+            System.out.format("Avoid transform %s with class loader %s. Only transform %s in class loader %s%n", className, loader, this.className, this.classLoader);
+        }*/
 
-        return classfileBuffer;
-    }
-
-    private CtClass createNewClassInJenkinsUberClassLoader(ClassLoader loader, byte[] classfileBuffer) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException, IOException {
-        /*Class jenkinsClass = loader.loadClass("jenkins.model.Jenkins");
-        Method factoryMethod = jenkinsClass.getDeclaredMethod("get");
-        Object jenkins = factoryMethod.invoke(null, null);
-
-        Field pluginManagerField = jenkinsClass.getDeclaredField("pluginManager");
-        Object pluginManager = pluginManagerField.get(jenkins);
-
-        Field uberClassLoaderField = pluginManager.getClass().getField("uberClassLoader");
-        Object uberClassLoader = uberClassLoaderField.get(pluginManager);
-
-*/
-        CtClass cl = null;
-        ClassPool classPool = ClassPool.getDefault();
-        cl = classPool.makeClass(new java.io.ByteArrayInputStream(classfileBuffer));
-
-        return cl;
+        return byteCode;
     }
 }
--- src/main/java/jenkins/telemetry/agents/TelemetryAgent.java
+++ src/main/java/jenkins/telemetry/agents/TelemetryAgent.java
@@ -1,42 +1,58 @@
 package jenkins.telemetry.agents;
 
 import java.lang.instrument.Instrumentation;
-import java.lang.instrument.UnmodifiableClassException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TelemetryAgent extends ClassNotFoundException {
-    private final static Logger logger = Logger.getLogger(TelemetryAgent.class.getName());
+    private final static Logger LOGGER = Logger.getLogger(TelemetryAgent.class.getName());
 
     public static void premain(String agentArguments, Instrumentation instrumentation) {
         RuntimeMXBean runtimeMxBean = ManagementFactory.getRuntimeMXBean();
-        logger.log(Level.INFO, "Runtime: {0}: {1}", new Object[]{runtimeMxBean.getName(), runtimeMxBean.getInputArguments()});
-        logger.log(Level.INFO, "Starting agent with arguments " + agentArguments);
+        LOGGER.log(Level.INFO, "Runtime: {0}: {1}", new Object[]{runtimeMxBean.getName(), runtimeMxBean.getInputArguments()});
+        LOGGER.log(Level.INFO, "Starting agent with arguments " + agentArguments);
 
-        // parse the arguments:
-        // graphite.host=localhost,graphite.port=2003
-      /*  Map<String, String> properties = new HashMap<String, String>();
-        for (String propertyAndValue : agentArguments.split(",")) {
-            String[] tokens = propertyAndValue.split(":", 2);
-            if (tokens.length != 2) {
-                continue;
-            }
-            properties.put(tokens[0], tokens[1]);
+        String classToTransform = "java.lang.ClassNotFoundException";
+        transformClass(classToTransform, instrumentation);
+    }
 
-        }*/
-        for (Class c : instrumentation.getAllLoadedClasses()){
-            System.out.println(c.getSimpleName());
-        }
-        instrumentation.addTransformer(new CatchExceptionTransformer(), true);
+    public static void agentmain(String agentArguments, Instrumentation instrumentation) {
+        premain(agentArguments, instrumentation);
+    }
+
+    private static void transformClass(String className, Instrumentation instrumentation) {
+        Class<?> targetCls = null;
+        ClassLoader targetClassLoader = null;
+        // see if we can get the class using forName
         try {
-            instrumentation.retransformClasses(java.lang.ClassNotFoundException.class);
-        } catch (UnmodifiableClassException e) {
-            //TODO: change for something more appropriate
-            e.printStackTrace();
+            targetCls = Class.forName(className);
+            targetClassLoader = targetCls.getClassLoader();
+            transform(targetCls, targetClassLoader, instrumentation);
+            return;
+        } catch (Exception ex) {
+            LOGGER.log(Level.WARNING, "Class [{0}] not found with Class.forName", className);
+        }
+
+        // otherwise iterate all loaded classes and find what we want
+        for(Class<?> clazz: instrumentation.getAllLoadedClasses()) {
+            if(clazz.getName().equals(className)) {
+                targetCls = clazz;
+                targetClassLoader = targetCls.getClassLoader();
+                transform(targetCls, targetClassLoader, instrumentation);
+                return;
+            }
+        }
+        throw new RuntimeException("Failed to find class [" + className + "]");
+    }
+
+    private static void transform(Class<?> clazz, ClassLoader classLoader, Instrumentation instrumentation) {
+        instrumentation.addTransformer(new CatchExceptionTransformer(clazz.getName(), classLoader), true);
+        try {
+            instrumentation.retransformClasses(clazz);
+        } catch (Exception ex) {
+            throw new RuntimeException("Transform failed for: [" + clazz.getName() + "]", ex);
         }
     }
 }
